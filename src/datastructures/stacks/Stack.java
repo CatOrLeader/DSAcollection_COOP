6e38b1b58c69726a5e47f71963be967f6f75cff2
Artur <arturmuxutdinov@gmail.com>
2023-06-23 14:21:03 +0300

    Initial commit with the skeleton structure of the project

---
+++ src/datastructures/stacks/Stack.java
@@ -0,0 +1,17 @@
+package datastructures.stacks;
+
+/**
+ * All the functions which can be used in the stack implementation.
+ * @param <T> - data type which will be stored in the stack.
+ */
+interface Stack<T> {
+    int size();
+
+    boolean isEmpty();
+
+    void push(T obj);
+
+    void pop();
+
+    T peek();
+}
