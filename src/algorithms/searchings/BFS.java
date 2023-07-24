edb521af47ba50ee494c2d9f283567917ebf2ee4
Artur <arturmuxutdinov@gmail.com>
2023-07-25 00:02:04 +0300

    feat(graph-searching):
    Added BFS and DFS

---
+++ src/algorithms/searchings/BFS.java
@@ -0,0 +1,70 @@
+package algorithms.searchings;
+
+import datastructures.graphs.GraphADT;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BFS<N, E extends Comparable<E>> {
+    public void breadthFirstSearchStartVertex(GraphADT<N, E> graph) {
+        int numVertices = graph.nodeCount();
+        ArrayList<N> nodes = graph.nodes();
+
+        boolean[] visited = new boolean[numVertices];
+
+        Queue<N> queue = new LinkedList<>();
+
+        int startVertex = 0;
+        visited[startVertex] = true;
+        queue.add(nodes.get(startVertex));
+
+        while (!queue.isEmpty()) {
+            N currentNode = queue.poll();
+            int currentIndex = nodes.indexOf(currentNode);
+
+            System.out.println(currentIndex + " ");
+
+            List<N> adjacent = graph.adjacentNodes(currentNode);
+
+            for (N vertex : adjacent) {
+                int vertexIndex = nodes.indexOf(vertex);
+                if (!visited[vertexIndex]) {
+                    visited[vertexIndex] = true;
+                    queue.add(vertex);
+                }
+            }
+        }
+    }
+
+    public void breadthFirstSearch(GraphADT<N, E> graph, N node) {
+        int numVertices = graph.nodeCount();
+        ArrayList<N> nodes = graph.nodes();
+
+        boolean[] visited = new boolean[numVertices];
+
+        Queue<N> queue = new LinkedList<>();
+
+        int startVertex = nodes.indexOf(node);
+        visited[startVertex] = true;
+        queue.add(nodes.get(startVertex));
+
+        while (!queue.isEmpty()) {
+            N currentNode = queue.poll();
+            int currentIndex = nodes.indexOf(currentNode);
+
+            System.out.println(currentIndex + " ");
+
+            List<N> adjacent = graph.adjacentNodes(currentNode);
+
+            for (N vertex : adjacent) {
+                int vertexIndex = nodes.indexOf(vertex);
+                if (!visited[vertexIndex]) {
+                    visited[vertexIndex] = true;
+                    queue.add(vertex);
+                }
+            }
+        }
+    }
+}
---
+++ src/algorithms/searchings/DFS.java
@@ -0,0 +1,34 @@
+package algorithms.searchings;
+
+import datastructures.graphs.GraphADT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFS<N, E extends Comparable<E>> {
+    public void depthFirstSearchStartVertex(GraphADT<N, E> graph) {
+        int numVertices = graph.nodes().size();
+
+        boolean[] visited = new boolean[numVertices];
+
+        int startIndex = 0;
+        recursive(graph, startIndex, visited);
+    }
+
+    private void recursive(GraphADT<N, E> graph, int currentIndex, boolean[] visited) {
+        visited[currentIndex] = true;
+        System.out.println(currentIndex + " ");
+
+        ArrayList<N> nodes = graph.nodes();
+        N curNode = nodes.get(currentIndex);
+
+        List<N> adjacent = graph.adjacentNodes(curNode);
+
+        for (N vertex : adjacent) {
+            int vertexIndex = nodes.indexOf(vertex);
+            if (!visited[vertexIndex]) {
+                recursive(graph, vertexIndex, visited);
+            }
+        }
+    }
+}
